c1fc8a6cfde5009961fca0c27ae1ec0122bef25a
JORDAN6ho <hoj738009@gmail.com>
2025-11-06 15:38:56 +0100

    Add files via upload

---
+++ JORDAN_HO/TP2ex8.py
@@ -0,0 +1,11 @@
+
+x = float(input("Entrez un nb réel :"))
+if x >=2 and x<3 :
+    print("x appartient à I")
+else :
+    if x >= 0 and x <= 1:
+        print("x appartient à I")
+    else :
+        if x >= -10 and x <= 2 :
+            print("x appartient à I")
+print("x n'appartient pas à I")
