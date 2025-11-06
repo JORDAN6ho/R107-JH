241b2ccd696addcc7f0e4c19505790629c797916
e2506418 <hoj738109@gmail.com>
2025-11-06 16:05:20 +0100

    slmldmls

---
+++ TP/TP2_swapex3.py
@@ -0,0 +1,26 @@
+a=input("Entrez la premiere  valeur : ")
+b=input("Entrez la deuxieme  valeur : ")
+c=input("Entrez la troisieme valeur : ")
+
+print("Les valeurs entrees sont : a = " + a + ", b = " + b + " et c = " + c)
+print("Permutation: a ==> b, b ==> c, c ==> a")
+"""      *******************************************
+         * Completez le programme a partir d'ici.
+         *******************************************
+"""
+temp = 0
+temp2 = 0
+
+temp = b
+temp2 = a
+a = c
+b = temp2
+c = temp
+
+
+"""     *******************************************
+         * Ne rien modifier apres cette ligne.
+         *******************************************
+"""
+
+print("Les valeurs permutees sont : a = " + a + ", b = " + b + " et c = " + c)
