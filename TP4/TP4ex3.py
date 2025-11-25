3b775a58a30c7192b810e3a373f260081f78f42b
e2506418 <hoj738009@gmail.com>
2025-11-25 15:26:20 +0100

    vb

---
+++ TP4/TP4ex3.py
@@ -0,0 +1,6 @@
+nMax = 3
+v1 = []
+v2 = []
+x = int(input("Quelle est la taille de vos vecteurs [entre 1 et 10] ?"))
+while 1 < x or x < nMax :
+    x = int(input("Quelle est la taille de vos vecteurs [entre 1 et 10] ?"))
