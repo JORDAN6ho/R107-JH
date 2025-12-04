2075ac629ed9b5e50a3aa1f6868a26d4f1f5a2ea
e2506418 <hoj738009@gmail.com>
2025-12-04 16:13:16 +0100

    sbr

--- TP4/TP4ex4.py
+++ TP4/TP4ex4.py
@@ -6,22 +6,15 @@ L1 = [2, 7, 5, 6, 7, 1, 6, 2, 1, 7, 6]
 
 courant = L1[0]
 autre = 1
-
 for i in range(len(L1)):
     cherche = L1[i]
-
     c = 0
     for i in range(1, len(L1)) :
         if L1[i] == cherche :
             c = c + 1
-
     if c > autre :
         autre = c
         courant = cherche
-
-
-
-
 print(f"Le nombre le plus fréquent dans la liste est le : {courant} ({autre} x)")
 
 
