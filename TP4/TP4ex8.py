7f112ff1de650085f4813b7c463b408f3487098f
e2506418 <ho738009@gmail.com>
2025-12-05 11:47:47 +0100

    sbr

--- TP4/TP4ex8.py
+++ TP4/TP4ex8.py
@@ -1,12 +1,6 @@
-dic = {
-    "firstname": "titi",
-    "name": "toto",
-    "promo": 2022,
-    "group": 202
-}
+dic = {"firstname": "titi","name": "toto","promo": 2022,"group": 202}
 print(f"votre nom est '{dic['name']}', prénom est '{dic['firstname']}', "
-      f"vous faites partie de la promo '{dic['promo']}' et votre groupe est '{dic['group']}'.")
-
+      f""f"vous faites partie de la promo '{dic['promo']}' et votre groupe est '{dic['group']}'.")
 print("Les clés du dictionnaire sont :")
 for c in dic.keys():
     print("-", c)
@@ -18,16 +12,8 @@ for t in dic.items():
     print("-", t)
 
 perso1 = dic
-perso2 = {
-    "firstname": "tata",
-    "name": "tutu",
-    "promo": 2023,
-    "group": 102
-}
-binome = {
-    "id1": perso1,
-    "id2": perso2
-}
+perso2 = {"firstname": "tata","name": "tutu","promo": 2023,"group": 102}
+binome = {"id1": perso1,"id2": perso2}
 print("Les étudiants formants le binôme sont :")
 for etu in binome.values():
     print(f"- L'étudiant {etu['name']} {etu['firstname']} du groupe {etu['group']}")
